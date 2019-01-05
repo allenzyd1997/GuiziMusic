59da3450364bc220c7400265ff71a0c10e105709
allenzyd1997 <allenwzyd@icloud.com>
2019-01-05 20:12:09 +0800

    在写播放界面

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
+
     <div class="header-wrap">
       <MHeader v-if="header_show"></MHeader>
       <Tab></Tab>
     </div>
-    <router-view></router-view>
+    <router-view v-on:public_header="public_header" v-on:pubic_footer="public_footer"></router-view>
     <MFooter  v-if="footer_show"></MFooter>
+
   </div>
 </template>
 
@@ -13,21 +15,24 @@
 import MHeader from '@/components/m-header/m-header'
 import MFooter from '@/components/m-footer/m-footer'
 import Tab from '@/components/tab/tab'
+import play from '@/components/play/play'
 
 export default {
 
   name: "app",
   data(){
       return{
-      header_show:true,
-      footer_show:true,
+        header_show:true,
+        footer_show:true,
     }
   },
   components: {
     MHeader,
     MFooter,
-    Tab
-  },
+    Tab,
+    play
+  }
+  
 }
 </script>
 
---
+++ src/components/play/play.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="play">
+		
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+	export default{
+
+		name: 'play',
+		components:{
+
+		},
+		data(){
+			return{
+				
+			}
+		},
+		function(){
+			this.$emit('public_header', false);
+			this.$emit('public_footer', false);
+		}
+	}
+</script>
